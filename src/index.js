d9d70ea37b9617e365664c19d80ce49d3bc3a4cb
Youssef Gamil <yoga1290@gmail.com>
2019-02-13 20:22:00 +0200

    WIP: need to find a way to stop the stream; ffmpeg throws error on 404

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import {
   FFmpegBridge
 } from './FFmpegBridge.js'
 
+import {
+  H5CanvasService
+} from './H5CanvasService.js'
+
 console.log(`FFMPEG installed at ${require('@ffmpeg-installer/ffmpeg').path}`);
 
 const namespace = '/namespace' //TODO
 
 app.use((req, res, next) => {
+  console.log('req.url', req.url)
+
   res.header('Access-Control-Allow-Origin', '*')
   next()
 })
@@ -22,13 +28,21 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/canvas', function(req, res) {
+  res.sendFile(__dirname + '/canvas.html');
+});
+
 // Range Requests
-app.get('/view',
-    new HTTPStreamIOBridge(io, namespace).getRangeResponse)
+// app.get('/view',
+//     new HTTPStreamIOBridge(io, namespace).getRangeResponse)
 
 // chunks
-app.get('/chunked',
-    new FFmpegBridge(io, namespace).getBufferChunkResponse)
+// app.get('/chunked',
+//     new FFmpegBridge(io, namespace).getBufferChunkResponse)
+
+app.get('/canvas/:img',
+  new H5CanvasService(io, '/canvas').getImage)
+    
 
 http.listen(3000, function(){
   // console.log('listening on *:3000');
